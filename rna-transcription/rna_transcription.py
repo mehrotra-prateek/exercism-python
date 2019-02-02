ab8e77fbf09e0788f6e1393d87d821e965f18092
Prateek Mehrotra <prateek.mehrotra@fil.com>
2019-02-02 13:20:04 +0530

    add excercise of rnatransalte

---
+++ rna-transcription/rna_transcription.py
@@ -0,0 +1,7 @@
+rnatrans = str.maketrans('GCTA', 'CGAU')
+
+def to_rna(dna_strand=""):
+    if type(dna_strand) is not str:
+        raise Exception('Not a string')
+    else:
+        return dna_strand.translate(rnatrans)
