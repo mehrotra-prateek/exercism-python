24deb4e6e13fcd73ce656970d07b4032097b6b85
Prateek Mehrotra <prateek.mehrotra@fil.com>
2019-10-20 11:56:30 +0530

    remove unused variables

--- space-age/space_age.py
+++ space-age/space_age.py
@@ -1,30 +1,28 @@
 class SpaceAge(object):
     def __init__(self, seconds):
-        self.seconds=seconds
-        self.age=((((self.seconds/60)/60)/24)/365.25)
-        pass
+        self.age=seconds/31557600.00
 
-    def on_earth(self,age_on_earth=0):
-        return(round(self.age, 2))
+    def on_earth(self):
+        return(round (self.age, 2))
 
-    def on_mercury(self, age_on_mercury=0):
+    def on_mercury(self):
         return(round(self.age/0.2408467, 2))
 
-    def on_venus(self, age_on_venus=0):
+    def on_venus(self):
         return(round(self.age/0.61519726, 2))    
     
-    def on_mars(self, age_on_mars=0):
+    def on_mars(self):
         return(round(self.age/1.8808158, 2))
 
-    def on_jupiter(self, age_on_jupiter=0):
+    def on_jupiter(self):
         return(round(self.age/11.862615, 2))    
 
-    def on_saturn(self, age_on_saturn=0):
+    def on_saturn(self):
         return(round(self.age/29.447498, 2))
 
-    def on_uranus(self, age_on_uranus=0):
+    def on_uranus(self):
         return(round(self.age/84.016846, 2))    
 
-    def on_neptune(self, age_on_neptune=0):
+    def on_neptune(self):
         return(round(self.age/164.79132, 2))    
 
