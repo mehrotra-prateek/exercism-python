e3371606c126f8ae53f5d44f1a308ad2162c0092
Prateek Mehrotra <prateek.mehrotra@fil.com>
2019-10-20 12:16:18 +0530

    refactor to create functions on runtime

--- space-age/space_age.py
+++ space-age/space_age.py
@@ -12,28 +12,9 @@ class SpaceAge(object):
     earth_year=31557600
 
     def __init__(self, seconds):
-        self.age_on_planet=seconds/self.earth_year
-
-    def on_mercury(self):
-        return(round(self.age_on_planet/self.relative_year_ratio_for_planet['mercury'], 2))
-
-    def on_venus(self):
-        return(round(self.age_on_planet/self.relative_year_ratio_for_planet['venus'], 2))    
-    
-    def on_earth(self):
-        return(round (self.age_on_planet/self.relative_year_ratio_for_planet['earth'], 2))
-
-    def on_mars(self):
-        return(round(self.age_on_planet/self.relative_year_ratio_for_planet['mars'], 2))
-
-    def on_jupiter(self):
-        return(round(self.age_on_planet/self.relative_year_ratio_for_planet['jupiter'], 2))    
-
-    def on_saturn(self):
-        return(round(self.age_on_planet/self.relative_year_ratio_for_planet['saturn'], 2))
-
-    def on_uranus(self):
-        return(round(self.age_on_planet/self.relative_year_ratio_for_planet['uranus'], 2))    
-
-    def on_neptune(self):
-        return(round(self.age_on_planet/self.relative_year_ratio_for_planet['neptune'], 2))
+        self.age_on_earth=seconds/self.earth_year
+        for planet in self.relative_year_ratio_for_planet:
+            setattr(self, "on_"+planet, self.age_on_planet(planet))
+            
+    def age_on_planet(self, planet):        
+        return lambda: round(self.age_on_earth/self.relative_year_ratio_for_planet[planet], 2)
